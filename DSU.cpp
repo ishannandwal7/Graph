5a6527b3040e10d5ee6d9424ccfe74a604668767
Ishan Nandwal <ishan.nandwal@lji.io>
2024-11-09 00:15:00 +0530

    Fixing DSU Class

--- DSU.cpp
+++ DSU.cpp
@@ -1,66 +1,50 @@
-class DSU{
+class DisjointSet {
+    vector<int> rank, parent, size;
 public:
-    int nodes;
-    vector<int>rank,parent,size;
-    DSU(int nodes){
-        this->nodes = nodes;
-        rank.resize(nodes+1);
-        parent.resize(nodes+1);
-        size.resize(nodes+1,1);
-        for(int i=1;i<=nodes;++i){
-            parent[i]=i;
-            rank[i]=0;
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
         }
     }
-    int findParent(int node){
-        // while(parent[node]!=node){
-        //     node=parent[node];
-        // }
-        // return node;
 
-        if(node==parent[node]){
+    int findUPar(int node) {
+        if (node == parent[node])
             return node;
-        }
-        return parent[node]=findParent(parent[node]);
-    } 
+        return parent[node] = findUPar(parent[node]);
+    }
 
-    void makeUnion(int u,int v){
-        int ultimateParentU = findParent(u);
-        int ultimateParentV = findParent(v);
-        int rankU = rank[u];
-        int rankV = rank[v];
-        if(ultimateParentU == ultimateParentV){return ;}
-        if(rankU==rankV){
-            parent[v]=ultimateParentU;
-            rank[u]++;
-        }else{
-            if(rankV<rankU){
-                parent[v]=ultimateParentU;
-            }else{
-                parent[u]=ultimateParentV;
-            }
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
         }
     }
 
-    void unionBySize(int u,int v){
-        int ultimateParentU = findParent(u);
-        int ultimateParentV = findParent(v);
-        if(ultimateParentU == ultimateParentV){return ;}
-        if(size[u]<size[v]){
-            parent[u]=ultimateParentV;
-            size[v]+=size[u];
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
         }
-        else{
-            if(size[u]==size[v]){
-                parent[u]=ultimateParentV;
-                size[v]+=size[v];
-            }
-            else{
-                parent[v]=ultimateParentU;
-                size[u]+=size[v];
-            }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
         }
-
     }
 };
 
